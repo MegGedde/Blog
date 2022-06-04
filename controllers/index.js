7036c07ec3f7a77a2c2485014dca0ed376ce44b9
Megan Gedde <hoglu070@d.umn.edu>
2022-06-04 15:09:26 -0500

    added controller routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,17 @@
-// const router = require('express').Router();
+const router = require('express').Router();
 
-// const userRoutes = require('./user-routes.js');
-// const postRoutes = require('./post-routes');
-// const commentRoutes = require('./comment-routes');
-
-// router.use('/users', userRoutes);
-// router.use('/posts', postRoutes);
-// router.use('/comments', commentRoutes);
-
+const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/', homeRoutes);
 
+router.use('/api', apiRoutes);
 
-// module.exports = router;
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
